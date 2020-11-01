cc9dd77df42d7ae7a263314b8ea6d4a5a0575c0e
Jacob <cobymstevens44@gmail.com>
2020-11-01 17:11:21 -0500

    Imported OS and SYS libraries, will only run with 2 arguments, Listing directories

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import os
-path = './Top'
-os.chdir(path)
-print(os.listdir(path)
+import sys
+
+if not(len(sys.argv) == 2):
+    print("Not Correct amount of args")
+    exit()
+
+path = sys.argv[1]
+print("Directory:",path)
+print("Files in Directory:",os.listdir(path=path))
+
+#os.chdir(path)
+#print(os.listdir(path)

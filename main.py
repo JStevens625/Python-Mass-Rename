ac54cbaa71f4f0306e8cb19b73733880c7b51794
Jacob <cobymstevens44@gmail.com>
2020-11-01 18:30:12 -0500

    File Rename implemented, folders renamed with parent folder

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import os
 import sys
 
-if not(len(sys.argv) == 2):
-    print("Not Correct amount of args")
-    exit()
-
-path = sys.argv[1]
-print("Directory:",path)
-print("Files in Directory:",os.listdir(path=path))
-
-#os.chdir(path)
-#print(os.listdir(path)
+if (len(sys.argv) == 2):
+    os.chdir(sys.argv[1])
+    appendName = os.getcwd().split('\\')[len(os.getcwd().split('\\'))-1]
+    for folder in os.listdir():
+        newFolderName = folder +"_"+ appendName
+        os.rename(folder,newFolderName)
